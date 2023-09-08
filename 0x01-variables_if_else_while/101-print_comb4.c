2f9764f7a958581f97e49d89b786b94da64d253d
Nellyjohn <okpala_vivian@.com>
2023-09-08 12:20:35 -0700

    I wrote  a program that prints all possible different combinations of three digits using putchar function

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -13,20 +13,22 @@ int main(void)
 
 	for (digit1 = 48; digit1 <= 55; digit1++)
 	{
-		for (digit2 = 49; digit2 <= 56; digit2++)
+		for (digit2 = digit1 + 1; digit2 <= 56; digit2++)
 		{
-			for (digit3 = 50; digit3 <= 57; digit3++)
+			for (digit3 = digit2 + 1; digit3 <= 57; digit3++)
 			{
-				if (digit3 <= 57)
-				{
-					putchar(',');
-					putchar(' ');
-				}
+				putchar(digit1);
+				putchar(digit2);
 				putchar(digit3);
+
+				if (digit1 == 55 && digit2 == 56 && digit3 == 57)
+				{
+					continue;
+				}
+				putchar(',');
+				putchar(' ');
 			}
-			putchar(digit2);
 		}
-		putchar(digit1);
 	}
 	putchar('\n');
 
