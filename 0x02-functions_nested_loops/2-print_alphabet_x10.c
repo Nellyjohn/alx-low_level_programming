3e9d091ff5f83905490284292c3c903bbf6e30f7
Nellyjohn <okpala_vivian@.com>
2023-09-12 05:45:43 -0700

    I wrote a function that prints 10 times the alphabet, in lowercase suing custom putchar function

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+  * print_alphabet_x10 - prints the alphabet 10 times
+  * Return: void
+  */
+
+void print_alphabet_x10(void)
+{
+	char loweralpha;
+	int count;
+
+	count = 0;
+	while (count <= 10)
+	{
+		loweralpha = 'a';
+		while (loweralpha <= 'z')
+		{
+			_putchar(loweralpha);
+			loweralpha++;
+		}
+		_putchar('\n');
+		count++;
+	}
+}
