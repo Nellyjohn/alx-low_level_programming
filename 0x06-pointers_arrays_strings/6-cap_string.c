55eaef3a9d05aff2d12b4d327a1b95697231fd7c
Nellyjohn <okpala_vivian@.com>
2023-09-21 07:47:31 -0700

    Debugging

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
-  * _isalpha - checks for alphabet
-  * @index: character to check
-  * Return: 1 if character, 0 otherwise
-  */
+ * _isalpha - checks for alphabet
+ * @index: character to check
+ * Return: 1 if character, 0 otherwise
+ */
 
 int _isalpha(int index)
 {
@@ -19,10 +19,28 @@ int _isalpha(int index)
 }
 
 /**
-  * _toupper - converts character to uppercase
-  * @index: character
-  * Return: coverted character
-  */
+ * _isdigit - checks for a digit from 0 to 9
+ * @index: digit
+ * Return: 1 if c is a digit, otherwise 0
+ */
+
+int _isdigit(int index)
+{
+	if (index >= 48 && index <= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+
+/**
+ * _toupper - converts character to uppercase
+ * @index: character
+ * Return: coverted character
+ */
 
 char _toupper(char index)
 {
@@ -35,10 +53,10 @@ char _toupper(char index)
 }
 
 /**
-  * _tolower - converts to lower character
-  * @index: character
-  * Return: converted character
-  */
+ * _tolower - converts to lower character
+ * @index: character
+ * Return: converted character
+ */
 
 char _tolower(char index)
 {
@@ -51,10 +69,10 @@ char _tolower(char index)
 }
 
 /**
-  * cap_string - capitalizes string
-  * @sentence: sentence to check
-  * Return: string
-  */
+ * cap_string - capitalizes string
+ * @sentence: sentence to check
+ * Return: string
+ */
 
 char *cap_string(char *sentence)
 {
@@ -64,7 +82,19 @@ char *cap_string(char *sentence)
 	index = 0;
 	while (sentence[index] != '\0')
 	{
-		if (_isalpha(sentence[index]))
+		if (_isalpha(sentence[index]) || _isdigit(sentence[index]))
+		{
+			if (capitalize_next)
+			{
+				sentence[index] = _toupper(sentence[index]);
+				capitalize_next = 0;
+			}
+			else
+			{
+				sentence[index] = _tolower(sentence[index]);
+			}
+		}
+		else if (_isalpha(sentence[index]))
 		{
 			if (capitalize_next)
 			{
