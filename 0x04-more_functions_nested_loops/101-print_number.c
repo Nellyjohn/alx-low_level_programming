a4a207d4b59bd4ef0b3909c2246cabbca86be1a3
Nellyjohn <okpala_vivian@.com>
2023-09-22 09:05:32 -0700

    I wrote a function that prints an integer

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,6 +1,24 @@
 #include "main.h"
 
 /**
+ * _abs - computes the absolute value of an integer
+ * @n: integer
+ * Return: integer value
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+}
+
+/**
  * print_number - prints an integer
  * @n: integer
  * Return: void
@@ -8,17 +26,13 @@
 
 void print_number(int n)
 {
-	if ( n == 0)
+	if (n < 0)
 	{
-		_putchar(48);
+		_putchar('-');
+		n = _abs(n);
 	}
-	else
-	{
-		n = n + 1;
-		while (n >= 0)
-		{
-			_putchar(n);
-			n++;
-		}
-	}
+	if ((n / 10) > 0)
+		print_number(n / 10);
+
+	_putchar(n % 10 + '0');
 }
