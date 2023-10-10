587df1e815495b04f4d57867a20643bcd5ef1039
Nellyjohn <okpala_vivian@.com>
2023-10-10 10:44:39 -0700

    Defined a new type struct dog with three members

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+#include <stdio.h>
+
+/**
+  * struct dog - short description
+  * @name: first memeber
+  * @age: second memeber
+  * @owner: third member
+  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
