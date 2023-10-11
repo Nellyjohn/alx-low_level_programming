a73f4f8009d854b264ccccc2c4ec60316a62fac0
Nellyjohn <okpala_vivian@.com>
2023-10-11 02:49:39 -0700

    I wrote a function that prints a name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+
+/**
+  * print_name - prints a nmae
+  * @name: name
+  * @f: pointer to function
+  * Return: nothing
+  */
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
