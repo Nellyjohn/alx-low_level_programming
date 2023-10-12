310a6ab84212bf215ba3386cc35269792e6c9c6c
Nellyjohn <okpala_vivian@.com>
2023-10-12 08:52:14 -0700

    Debugging

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,12 +1,13 @@
 #include "function_pointers.h"
 
 /**
-  * print_name - prints a nmae
-  * @name: name
-  * @f: pointer to function
-  * Return: nothing
-  */
+ * print_name - prints a nmae
+ * @name: name
+ * @f: pointer to function
+ * Return: nothing
+ */
 void print_name(char *name, void (*f)(char *))
 {
-	(*f)(name);
+	if (name && f)
+		(*f)(name);
 }
