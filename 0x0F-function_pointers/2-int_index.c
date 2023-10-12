758492e753d8161b2145aab7a26449ad6c7f765e
Nellyjohn <okpala_vivian@.com>
2023-10-12 09:00:41 -0700

    Debugging

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,12 +1,12 @@
 #include "function_pointers.h"
 
 /**
-  * int_index - searches for an integer
-  * @array: array
-  * @size: size of the array
-  * @cmp: function pointer to int_index
-  * Return: nothing
-  */
+ * int_index - searches for an integer
+ * @array: array
+ * @size: size of the array
+ * @cmp: function pointer to int_index
+ * Return: nothing
+ */
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int index = 0;
@@ -14,11 +14,14 @@ int int_index(int *array, int size, int (*cmp)(int))
 	if (size <= 0)
 		return (-1);
 
-	while (index < size)
+	if (array && cmp)
 	{
-		if (cmp(array[index]) != 0)
-			return (index);
-		index++;
+		while (index < size)
+		{
+			if (cmp(array[index]) != 0)
+				return (index);
+			index++;
+		}
 	}
 	return (-1);
 }
