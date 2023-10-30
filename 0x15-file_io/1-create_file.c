4fd25f429bbd43b0a21f5e5b872a8c03dd4df3ad
Nellyjohn <okpala_vivian@.com>
2023-10-30 07:20:10 -0700

    Function that creates a file.

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,11 +13,11 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_RDWR, S_IRUSR | S_IWUSR | O_TRUNC);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 
-	if (!text_content)
+	if (text_content != NULL)
 	{
 		len = 0;
 		while (text_content[len] != '\0')
