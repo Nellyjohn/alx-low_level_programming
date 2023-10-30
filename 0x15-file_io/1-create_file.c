5dc51a18d2e5df757c03785bb849d46109fa419c
Nellyjohn <okpala_vivian@.com>
2023-10-30 11:40:47 -0700

    Update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,7 +13,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 
@@ -22,13 +22,11 @@ int create_file(const char *filename, char *text_content)
 		len = 0;
 		while (text_content[len] != '\0')
 			len++;
+		bytes_written = write(fd, text_content, len);
+		if (bytes_written == -1)
+			return (-1);
 	}
 
-	bytes_written = write(fd, text_content, len);
-	if (bytes_written == -1)
-		return (-1);
-
 	close(fd);
-
 	return (1);
 }
